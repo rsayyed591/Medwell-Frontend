6b92ca00172dadc2e3e220b46d00ffc6068493c6
Nishikant00 <githubnsr@gmail.com>
2024-09-28 23:43:56 +0530

    react

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vercel from 'vite-plugin-vercel';
- 
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   server: {
     port: process.env.PORT,
