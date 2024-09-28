817b9c2d7d174f490b4fc64e86a3e62a5e853b06
Nishikant00 <githubnsr@gmail.com>
2024-09-28 23:48:38 +0530

    env

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    port: process.env.PORT,
-  },
-  plugins: [vercel(),react()],
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    },
+    plugins: [vercel(),react()],
 });
