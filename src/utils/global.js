aa0f582db91af329dce9a01c0e81d809118e22c2
rsayyed591 <rehansayyed591@gmail.com>
2024-10-29 00:30:51 +0530

    ngg

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,3 +1,3 @@
-export const ngrok_url="https://3d42-43-231-238-206.ngrok-free.app"
+export const ngrok_url="https://9271-43-231-238-206.ngrok-free.app"
 
-export const google_ngrok_url="https://3d42-43-231-238-206.ngrok-free.app"
+export const google_ngrok_url="https://9271-43-231-238-206.ngrok-free.app"
